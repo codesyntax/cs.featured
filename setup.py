4dfffdb5a0b397cdfda17cbbf78f435c734b64eb
Mikel Larreategi <mlarreategi@codesyntax.com>
2013-02-14 11:50:05 +0100

    Updating version on trunk after tagging

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ This module contains the tool of cs.featured
 import os
 from setuptools import setup, find_packages
 
-version = '1.0'
+version = '1.1'
 
 setup(name='cs.featured',
       version=version,
